485b05aeef80ba4bec555486b2578f600ab400db
Alberto Barrera <Artyom2217>
2021-11-15 19:26:02 -0600

    Day one

---
+++ Day_1/E1.py
@@ -0,0 +1,22 @@
+#string
+string1= "Hello world"
+#Int
+int1 = 3
+num1 = 3
+#float
+float1 = 3.1416
+#Boolean
+flag1 = True
+flag2 = False 
+#
+"""print(f"num1 = {num1}")
+print(f"Flag = {flag1}")"""
+
+"""string2 = input("Type a new string: ")
+print(string2)"""
+
+a = int(input("Type a number: "))
+b = int(input("Type a number: "))
+
+c = a ** b 
+print(c)
---
+++ Day_1/E2.py
@@ -0,0 +1,6 @@
+print("Welcome to the band name generator")
+city = (input("What's the neme of the city where you graw up in?: "))
+pet = (input("What's your pet name: "))
+
+band_name = city + " " + pet
+print(f"Your Band name could be {band_name}")
